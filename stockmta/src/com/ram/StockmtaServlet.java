4a5ab15c087d69e9b5003e24e392ca48bee1172e
Hilarm <hilarm@mail.mta.ac.il>
2015-03-08 15:50:38 +0200

    Initial commit.

---
+++ stockmta/src/com/ram/StockmtaServlet.java
@@ -0,0 +1,13 @@
+package com.ram;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StockmtaServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
